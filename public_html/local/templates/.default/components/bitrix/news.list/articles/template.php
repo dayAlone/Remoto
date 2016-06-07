5df83b1c612c484fe29998fe907daf139e6246db
dayAlone <andrey.slider@gmail.com>
2016-06-07 14:48:51 +0700

    Articles

--- public_html/local/templates/.default/components/bitrix/news.list/articles/template.php
+++ public_html/local/templates/.default/components/bitrix/news.list/articles/template.php
@@ -4,7 +4,7 @@
     <div class="articles__item">
         <a href="#<?=$item['CODE']?>" class="articles__title">
             <div class="articles__icon"><img src="<?=$item['PREVIEW_PICTURE']['SRC']?>"></div>
-            <div class="articles__name"> <span><?=$item['NAME']?></span></div>
+            <div class="articles__name"> <span><?=strip_tags($item['~NAME'])?></span></div>
         </a>
         <div class="articles__description"><?=$item['~PREVIEW_TEXT']?></div>
     </div>
