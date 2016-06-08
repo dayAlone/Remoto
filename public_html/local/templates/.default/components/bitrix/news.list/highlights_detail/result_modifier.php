e5a8aff83bf811d7e02eaba2af11b64de1dd6e84
dayAlone <andrey.slider@gmail.com>
2016-06-08 23:40:53 +0700

    Resize

--- public_html/local/templates/.default/components/bitrix/news.list/highlights_detail/result_modifier.php
+++ public_html/local/templates/.default/components/bitrix/news.list/highlights_detail/result_modifier.php
@@ -1,7 +1,10 @@
 <?
 $items = array();
-$item['DETAIL_PICTURE']['SMALL'] = CFile::ResizeImageGet($item['DETAIL_PICTURE']['ID'], Array("width" => 800, "height" => 700), BX_RESIZE_IMAGE_PROPORTIONAL, false, false, false, 80);
-foreach ($arResult['ITEMS'] as $item) array_merge($items, $item['PROPERTIES']['FEATURES']['VALUE']);
+
+foreach ($arResult['ITEMS'] as &$item) {
+    array_merge($items, $item['PROPERTIES']['FEATURES']['VALUE']);
+    $item['DETAIL_PICTURE']['SMALL'] = CFile::ResizeImageGet($item['DETAIL_PICTURE']['ID'], Array("width" => 800, "height" => 700), BX_RESIZE_IMAGE_PROPORTIONAL, false, false, false, 80);
+}
 $arSelect = Array("ID", "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT");
 $arFilter = Array("IBLOCK_ID"=>6, 'ID' => $items);
 $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
