d1cc65d69512ecedcf00076b7c62c7bbf9810e92
dayAlone <andrey.slider@gmail.com>
2016-06-08 12:24:47 +0700

    Slider

--- public_html/local/templates/.default/components/bitrix/news.detail/slider/template.php
+++ public_html/local/templates/.default/components/bitrix/news.detail/slider/template.php
@@ -2,7 +2,7 @@
 $item = $arResult;
 $s = end($arResult['SECTION']['PATH']);
 ?>
-<div class='block__image fotorama' data-nav='false' data-autoplay='3000' data-transition='crossfade' data-loop='true' data-fit="scaledown">
+<div class='block__image fotorama' data-nav='false' data-arrows="false" data-autoplay='1500' data-transition='dissolve' data-loop='true' data-fit="scaledown">
 	<?foreach ($item["PROPERTIES"]["IMAGES"]['VALUE'] as $img):?>
       <img src="<?=CFile::GetPath($img)?>" alt="" />
     <?endforeach;?>
