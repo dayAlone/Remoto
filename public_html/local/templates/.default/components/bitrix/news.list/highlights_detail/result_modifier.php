15e9b0b96dd642a7d83cc3b0cd1aac4d687f6bb6
dayAlone <andrey.slider@gmail.com>
2016-06-08 23:45:32 +0700

    Changes

--- public_html/local/templates/.default/components/bitrix/news.list/highlights_detail/result_modifier.php
+++ public_html/local/templates/.default/components/bitrix/news.list/highlights_detail/result_modifier.php
@@ -3,7 +3,7 @@ $items = array();
 
 foreach ($arResult['ITEMS'] as &$item) {
     array_merge($items, $item['PROPERTIES']['FEATURES']['VALUE']);
-    $item['DETAIL_PICTURE']['SMALL'] = CFile::ResizeImageGet($item['DETAIL_PICTURE']['ID'], Array("width" => 800, "height" => 700), BX_RESIZE_IMAGE_PROPORTIONAL, false, false, false, 80);
+    $item['DETAIL_PICTURE']['SMALL'] = CFile::ResizeImageGet($item['DETAIL_PICTURE']['ID'], Array("width" => 800, "height" => 700), BX_RESIZE_IMAGE_PROPORTIONAL, false, false, false, 70);
 }
 $arSelect = Array("ID", "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT");
 $arFilter = Array("IBLOCK_ID"=>6, 'ID' => $items);
