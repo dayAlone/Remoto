51472884280263a59d3800e49e200eff461fa559
dayAlone <andrey.slider@gmail.com>
2016-06-10 01:51:56 +0700

    CSS

--- public_html/index.php
+++ public_html/index.php
@@ -77,7 +77,7 @@ $APPLICATION->SetPageProperty('body_class', 'page--index');
                 <div class='text__content'>
                     <h2 class="title"> <span>Now every car<br/></span>can be smart</h2>
                     <p><?=COption::GetOptionString("grain.customsettings","about") ?></p>
-                    <a href="#How" data-toggle='modal' class="button button--white">how does it work</a>
+                    <?/*<a href="#How" data-toggle='modal' class="button button--white">how does it work</a>*/?>
                     <br>
                 </div>
                 <div class='previews'>
